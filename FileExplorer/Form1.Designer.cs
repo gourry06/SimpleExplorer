5e34af90af1d3419c067f9fdc296752c73a32422
Iteration Increment <ehakdrktj@gmail.com>
2017-04-21 22:25:51 +0900

    Add files via upload

---
+++ FileExplorer/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace FileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chModifiedDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chFormat = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.ShowNodeToolTips = true;
+            this.treeView1.Size = new System.Drawing.Size(202, 666);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 75);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(933, 672);
+            this.splitContainer1.SplitterDistance = 208;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chModifiedDate,
+            this.chFormat,
+            this.chSize});
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(711, 666);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // chName
+            // 
+            this.chName.Text = "이름";
+            this.chName.Width = 290;
+            // 
+            // chModifiedDate
+            // 
+            this.chModifiedDate.Text = "수정한 날짜";
+            this.chModifiedDate.Width = 171;
+            // 
+            // chFormat
+            // 
+            this.chFormat.Text = "유형";
+            this.chFormat.Width = 143;
+            // 
+            // chSize
+            // 
+            this.chSize.Text = "크기";
+            this.chSize.Width = 101;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(202, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "상위 디렉토리 설정";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(223, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "검색";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(488, 60);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "※ 상위 디렉토리가 Root가 아닌 경로를 잡아야 검색 가능함. (확장자나 폴더 검색은 못함)\r\n\r\n※ 좌측 트리뷰에서는 현재 존재하는 하위 폴더" +
+    "를 보거나 선택할 수 있음. \r\n\r\n※ 리스트에 나열된 목록 중 하나를 더블클릭 하면 폴더or파일을 실행함.";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 750);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(957, 66);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 816);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(520, 480);
+            this.Name = "Form1";
+            this.Text = "파일 탐색기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chModifiedDate;
+        private System.Windows.Forms.ColumnHeader chSize;
+        private System.Windows.Forms.ColumnHeader chFormat;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ FileExplorer/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileExplorer
+{
+    public partial class Form1 : Form
+    {
+        string m_curPath = "";
+        Thread m_thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_curPath = "Root";
+            label1.Text = m_curPath;
+
+            TreeNode root = treeView1.Nodes.Add(m_curPath);
+
+            string[] drives = Directory.GetLogicalDrives();
+
+            foreach (string drive in drives)
+            {
+                DriveInfo di = new DriveInfo(drive);
+
+                if (di.IsReady)
+                {
+                    TreeNode node = root.Nodes.Add(drive);
+                    node.Nodes.Add("\\");
+                }
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text.Equals("\\"))
+                {
+                    e.Node.Nodes.Clear();
+
+                    string path = e.Node.FullPath.Substring(e.Node.FullPath.IndexOf("\\") + 1);
+
+                    string[] directories = Directory.GetDirectories(path);
+                    foreach (string directory in directories)
+                    {
+                        TreeNode newNode = e.Node.Nodes.Add(directory.Substring(directory.LastIndexOf("\\") + 1));
+                        newNode.Nodes.Add("\\" );
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("treeView1_BeforeExpand : " + ex.Message);
+            }
+        }
+
+        private void ViewDirectoryList(string path)
+        {
+            if (m_thread != null && m_thread.IsAlive)
+                m_thread.Abort();
+
+            string curPath = path;
+
+            Console.WriteLine(path.IndexOf("Root\\"));
+            if (path.IndexOf("Root\\") == 0)
+            {
+                curPath = path.Substring(path.IndexOf("\\") + 1);
+                label1.Text = (curPath.Length > 4) ? curPath.Remove(curPath.IndexOf("\\") + 1, 1) : curPath;
+                m_curPath = label1.Text;
+            }
+            else
+            {
+                label1.Text = path;
+                m_curPath = path;
+            }
+
+            try
+            {
+                listView1.Items.Clear();
+
+                string[] directories = Directory.GetDirectories(curPath);
+
+                foreach (string directory in directories)
+                {
+                    DirectoryInfo info = new DirectoryInfo(directory);
+                    ListViewItem item = new ListViewItem(new string[]
+                    {
+                        info.Name, info.LastWriteTime.ToString(), "파일 폴더", ""
+                    });
+                    listView1.Items.Add(item);
+                }
+
+                string[] files = Directory.GetFiles(curPath);
+
+                foreach (string file in files)
+                {
+                    FileInfo info = new FileInfo(file);
+                    ListViewItem item = new ListViewItem(new string[]
+                    {
+                        info.Name, info.LastWriteTime.ToString(), info.Extension, ((info.Length/1000)+1).ToString()+"KB"
+                    });
+                    listView1.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ViewDirectoryList : " + ex.Message);
+            }
+        }
+
+        private void SelectTreeView(TreeNode node)
+        {
+            if (node.FullPath == null)
+            {
+                Console.WriteLine("empth node.FullPath");
+                return;
+            }
+
+            string path = node.FullPath;
+
+            ViewDirectoryList(path);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            SelectTreeView(e.Node);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                m_curPath = fbd.SelectedPath;
+                Console.WriteLine(m_curPath);
+                label1.Text = m_curPath;
+
+                ViewDirectoryList(m_curPath);
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                string processPath;
+                if (listView1.SelectedItems[0].Text.IndexOf("\\") > 0)
+                    processPath = listView1.SelectedItems[0].Text;
+                else
+                    processPath = m_curPath + "\\" + listView1.SelectedItems[0].Text;
+
+                Process.Start("explorer.exe", processPath);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(m_thread != null && m_thread.IsAlive)
+                m_thread.Abort();
+
+            m_curPath = label1.Text;
+            DirectoryInfo rootDirInfo = new DirectoryInfo(m_curPath);
+            string searchFiles = textBox1.Text;
+
+            m_thread = new Thread(delegate ()
+            {
+                WalkDirectoryTree(rootDirInfo, searchFiles);
+            });
+            m_thread.Start();
+        }
+
+        public void WalkDirectoryTree(DirectoryInfo dirInfo, string searchFiles)
+        {
+            listView1.Items.Find(searchFiles, true);
+
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirs = null;
+           
+            try
+            {
+                files = dirInfo.GetFiles(searchFiles + "*.*");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (files != null)
+            {
+                DirectoryInfo tempDirInfo = new DirectoryInfo(m_curPath);
+
+                if (dirInfo.ToString() == tempDirInfo.ToString())
+                    listView1.Items.Clear();
+
+                foreach (FileInfo fi in files)
+                {
+                    ListViewItem item = new ListViewItem(new string[]
+                    {
+                        fi.FullName, fi.LastWriteTime.ToString(), fi.Extension, ((fi.Length/1000)+1).ToString()+"KB"
+                    });
+                    listView1.Items.Add(item);
+                }
+
+                subDirs = dirInfo.GetDirectories();
+                foreach (DirectoryInfo di in subDirs)
+                {
+                    WalkDirectoryTree(di, searchFiles);
+                }
+            }
+        }
+    }
+}
